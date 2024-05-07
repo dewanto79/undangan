7d48597557687e51dd2b35a34c4913035867d95f
jerriejayadi <tjiauw.jerrie@nanovest.io>
2024-05-07 21:49:09 +0800

    [ADD] Penutup and Footer section

--- src/app/content/page.tsx
+++ src/app/content/page.tsx
@@ -2,8 +2,10 @@
 
 import Doa from "@/components/layouts/Doa";
 import EventLocation from "@/components/layouts/EventLocation";
+import Footer from "@/components/layouts/Footer";
 import GaleriFoto from "@/components/layouts/GaleriFoto";
 import KirimHadiah from "@/components/layouts/KirimHadiah";
+import Penutup from "@/components/layouts/Penutup";
 import Perkenalan from "@/components/layouts/Perkenalan";
 import UcapanDanDoa from "@/components/layouts/UcapanDanDoa";
 import WeddingEvent from "@/components/layouts/WeddingEvent";
@@ -31,6 +33,10 @@ export default function Content() {
       </section>
       <section>
         <KirimHadiah />
+      </section>
+      <section>
+        <Penutup />
+        <Footer />
       </section>
     </div>
   );
---
+++ src/components/layouts/Penutup/index.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+export default function Penutup() {
+  return (
+    <div className={`text-center`}>
+      <div className={`font-montserrat`}>
+        Merupakan suatu kehormatan dan kebahagiaan bagi kami, apabila
+        Bapak/Ibu/Saudara/i berkenan hadir dan memberikan doa restu kepada kedua
+        mempelai. Atas doa restunya, kami mengucapkan terima kasih. Hormat kami
+        yang berbahagia
+      </div>
+      <div className={`font-euphoria text-4xl mt-6`}>
+        Dewanto Surya
+        <br />
+        &
+        <br />
+        Kiki Deya
+      </div>
+      <div className={`my-9`}>
+        <Image
+          className={`mx-auto`}
+          alt={``}
+          src={`/images/couple-cat 1.png`}
+          width={150}
+          height={250}
+        />
+      </div>
+    </div>
+  );
+}

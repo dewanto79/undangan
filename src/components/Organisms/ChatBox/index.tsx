805f15a93340a8fbb4f0580ecb7a2617661c37f3
jerriejayadi <tjiauw.jerrie@nanovest.io>
2024-05-09 20:17:24 +0800

    [FIX] Build Error

--- src/components/Organisms/ChatBox/index.tsx
+++ src/components/Organisms/ChatBox/index.tsx
@@ -86,6 +86,7 @@ export default function ChatBox({ isOpen, onClose, onSubmit }: ChatBoxProps) {
                     {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(
                       (rows, index: number) => (
                         <Image
+                          key={index}
                           onClick={(e) => {
                             setPayload((prev) => ({
                               ...prev,

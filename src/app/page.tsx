ce870b6116ca4a26f59d162b93367e106b08cdbd
jerriejayadi <tjiauw.jerrie@nanovest.io>
2024-05-09 22:42:40 +0800

    [FEAT] Play Audio when entering undangan page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,12 @@ import {
 } from "iconsax-react";
 
 export default function Home() {
-  const [state, setState] = useState<boolean>(true);
+  const [state, setState] = useState<boolean>(false);
   const [audioStatus, setAudioStatus] = useState<boolean>(false);
   const myRef = useRef<HTMLAudioElement>();
 
   const startAudio = () => {
     myRef.current?.play();
-
     setAudioStatus(true);
   };
 
@@ -30,29 +29,34 @@ export default function Home() {
     setAudioStatus(false);
   };
 
-  useEffect(() => {
-    state && startAudio();
-  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between font-montserrat max-w-[375px] mx-auto  ">
       <div className={`bg-cover h-full snap-y snap-mandatory`}>
-        <LandingPage className={``} state={state} setState={setState} />
+        <LandingPage
+          className={``}
+          state={state}
+          setState={setState}
+          playAudio={startAudio}
+        />
       </div>
       {state && <Content />}
-      <button
-        className={`fixed bottom-5 right-5 ${
-          audioStatus ? "bg-sky-200" : "bg-sky-100"
-        } p-2 rounded-lg text-xs`}
-        onClick={() => {
-          audioStatus ? pauseAudio() : startAudio();
-        }}
-      >
-        {audioStatus ? (
-          <VolumeHigh size={24} className={` text-sky-500 `} />
-        ) : (
-          <VolumeCross size={24} className={` text-sky-500 text-sky-300 `} />
-        )}
-      </button>
+      {state && (
+        <button
+          className={`fixed bottom-5 right-5 ${
+            audioStatus ? "bg-sky-200" : "bg-sky-100"
+          } p-2 rounded-lg text-xs`}
+          onClick={() => {
+            audioStatus ? pauseAudio() : startAudio();
+          }}
+        >
+          {audioStatus ? (
+            <VolumeHigh size={24} className={` text-sky-500 `} />
+          ) : (
+            <VolumeCross size={24} className={` text-sky-300 `} />
+          )}
+        </button>
+      )}
+
       <audio ref={myRef as any} src="/audio/bgm.mp3" />
     </main>
   );
